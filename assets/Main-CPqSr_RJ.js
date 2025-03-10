1c56cf1ca95b1823ecb934c50b8874dd1b230bae
edsonaf <edsonaf@users.noreply.github.com>
2025-03-10 12:18:31 +0000

    Deploying to gh-pages from  @ cdaafc5b3af90606a737606a7525638107f207e9 🚀

---
+++ assets/Main-CPqSr_RJ.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-CmUieqE6.js";function i(){return n.jsx("div",{children:n.jsx(r,{})})}function r(){return n.jsx("div",{children:"Welcome on Main Portal"})}export{i as default};
