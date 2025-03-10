2c8d7225282c4582782b1d7b80af09dcfc4d3d8e
edson <edson@yangasportswater.com>
2025-03-10 17:49:29 +0100

    add specPattern in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) { },
     baseUrl: 'http://localhost:4173',
+    specPattern: 'cypress/integration/**/*.cy.js',
     supportFile: 'cypress/support/e2e.ts'
   },
 })
