d3aab31e2461d20df8f527e40bad57cde476664b
edson <edson@yangasportswater.com>
2025-03-10 17:55:35 +0100

    fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) { },
     baseUrl: 'http://localhost:4173',
-    specPattern: 'cypress/integration/**/*.cy.js',
+    specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.ts'
   },
 })
