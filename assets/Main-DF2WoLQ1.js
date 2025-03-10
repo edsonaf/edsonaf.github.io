b11f0a33c39c9058335af101cc3569ad0c52351d
edsonaf <edsonaf@users.noreply.github.com>
2025-03-10 11:39:00 +0000

    Deploying to gh-pages from  @ 48e9577d537c041dd0c9ddce24ca7d389dca91a2 🚀

---
+++ assets/Main-DF2WoLQ1.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-C-LXBm_O.js";function i(){return n.jsx("div",{children:n.jsx(r,{})})}function r(){return n.jsx("div",{children:"Welcome on Main Portal"})}export{i as default};
